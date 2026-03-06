33f4f541523afb88a5df8ac42e7cc3f6b6ce8f8d
Victor Emanoel <victoremanoelao@gmail.com>
2026-03-06 14:43:07 -0300

    adding image swap function

---
+++ app.js
@@ -0,0 +1,6 @@
+function proximo(){
+    document.body.style.backgroundImage = "url('img/girassois.webp)"
+    document.getElementById("titulo").innerHTML = "girassóis"
+    document.getElementById("texto").innerHTML  = " agora estamos vendo a pintura Girassóis"
+
+}
